3ad5cd5b2073adcc5c4f159b679fe8aeaff733fb
Gary Zhu <ZHUGX@legal.regn.net>
2019-01-31 14:07:34 +0800

    add the extension class

---
+++ ExpressionToWhereClause/ExpressionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ExpressionToWhereClause
+{
+    public static class ExpressionExtensions
+    {
+        public static (string, Dictionary<string, object>) ToWhereClause<T>(this Expression<Func<T, bool>> expression) where T : class
+        {
+            ExpressionEntry expressionEntry = new ExpressionEntry();
+            expressionEntry.Visit(expression);
+            return expressionEntry.GetResult();
+        }
+    }
+}
